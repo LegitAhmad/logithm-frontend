ab554a3b54f6aef96d729090199fd967f346fcdd
LegitAhmad <a.s20061111@gmail.com>
2025-10-04 16:07:36 +0500

    make header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './layout/footer/footer';
+import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.sass'
+  styleUrl: './app.sass',
 })
 export class App {
   protected readonly title = signal('logithm-frontend');
