65c8ac40f1efa8ea4827c266f12c0ceb6f8563e5
LegitAhmad <a.s20061111@gmail.com>
2026-01-30 13:29:18 +0500

    login & signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Login } from './pages/auth/login/login';
+import { Signup } from './pages/auth/signup/signup';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
+  },
+  {
+    path: 'login',
+    title: 'Logithm | Login',
+    component: Login,
+  },
+  {
+    path: 'signup',
+    title: 'Logithm | Join Now',
+    component: Signup,
   },
 ];
